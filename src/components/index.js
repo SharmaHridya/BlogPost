d3f3814c5b35bd29fe1422d95a6f7571c784e9ce
Hridya Sharma <hridyasharma2529@gmail.com>
2026-06-24 01:48:04 +0530

    Post form and RTE components added

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,29 @@ import Header from "./Header/Header.jsx";
 import Container from "./Container.jsx"
 import Logo from "./Logo.jsx"
 import LogoutButton from "./Header/LogoutButton.jsx"
+import AuthLayout from "./AuthLayout.jsx"
+import Login from "../pages/Login.jsx"
+import Signup from "../pages/Signup.jsx"
+import Button from "./Button.jsx"
+import Input from "./input.jsx"
+import Select from "./Select.jsx"
+import RTE from "./RTE.jsx"
+import PostForm from "./post_form/PostForm.jsx"
+import PostCard from "./PostCard.jsx"
 
-export { Footer, Header, Container, Logo, LogoutButton }
+export {
+    Footer,
+    Header,
+    Container,
+    Logo,
+    LogoutButton,
+    AuthLayout,
+    Login,
+    Signup,
+    Button,
+    Input,
+    Select,
+    RTE,
+    PostForm,
+    PostCard
+}
